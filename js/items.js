096e897a080fb6346ca0fe10f520c2a9dc60b96a
estefaniaoc <ortizc.estefania@gmail.com>
2024-07-07 22:05:47 -0600

    Change: arreglo de imagenes

--- js/items.js
+++ js/items.js
@@ -1,48 +1,87 @@
-// Initialize a new TaskManager with currentId set to 0
 const itemsController = new ItemsController();
 
-function addItemCard(item){
-    const itemHTML = '<div class="card" style="width: 20rem;">\n' +
-        '    <img src="'+item.imageUrl +'" width="300" height="250"  alt="product image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
+function addItemCard(item) {
+    const itemHTML = `
+        <br>
+        <div class="col-card">
+        <div class="card" style="width: 20rem;">
+            <img src="${item.imageUrl}" width="300"  alt="product image">
+            <div class="card-body">
+                <h4 class="card-title">${item.name}</h4>
+                <!--<p class="card-text">${item.description}</p>-->
+                <p class="card-text content-center" >${item.price}</p>
+               <!-- <a href="#" class="btn btn-primary">Add</a>-->
+            </div>
+        </div>
+        </div>
+        <br/>
+    `;
     const itemsContainer = document.getElementById("list-items");
     itemsContainer.innerHTML += itemHTML;
-} 
+}
 
-function loadStorageSampleData(){
-    if(!localStorage.getItem("items")){
+function loadStorageSampleData() {
+    if (!localStorage.getItem("items")) {
         const sampleItems = [
             {
-                "name": "Body Damon",
-                "imageUrl": "/img/Body-Damon",
-                "description":"Body encaje vino",
-                "price":"$799.99"
+                name: "Body Damon",
+                imageUrl: "/img/Body-Damon.jpg",
+                description: "Body encaje vino",
+                price: "$799.99 MXN"
             },
             {
-                "name": "Body Carmina",
-                "imageUrl": "public/img/Body-Carmina.jpg",
-                "description":"Body encaje negro",
-                "price":"$799.99"
-
+                name: "Body Carmina",
+                imageUrl: "/img/BodyCarmina(2).jpg",
+                description: "Body encaje negro",
+                price: "$799.99 MXN"
             },
             {
-                "name": "Bustier Ale",
-                "imageUrl": "public/img/Bustier-Ale.jpg",
-                "description":"Bustier encaje negro",
-                "price":"799"
-                
+                name: "Bustier Ale",
+                imageUrl: "/img/Bustier-Ale.jpg",
+                description: "Bustier encaje negro",
+                price: "$799 MXN"
             },
             {
-                "name": "Set Angel",
-                "imageUrl": "public/img/Set-Angel.jpg",
-                "description":"Set de encaJE Angel",
-                "price":"799"
+                name: "Set Angel",
+                imageUrl: "/img/Set-Angel.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Daniela",
+                imageUrl: "/img/Set-Daniela.jpg",
+                description: "Set ",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Susan",
+                imageUrl: "/img/Set-Susan.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Vedette",
+                imageUrl: "/img/Set-Vedette.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Catalina",
+                imageUrl: "/img/Set-Catalina.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Florentina",
+                imageUrl: "/img/Set-Florentina.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
+            },
+            {
+                name: "Set Cordelia",
+                imageUrl: "/img/Set-Cordelia.jpg",
+                description: "Set de encaje Angel",
+                price: "$799 MXN"
             }
 
         ];
@@ -50,11 +89,9 @@ function loadStorageSampleData(){
     }
 }
 
-function loadCardsListFromItemsController(){
-    for(var i = 0, size = itemsController.items.length; i < size ; i++){
-        const item = itemsController.items[i];
-        addItemCard(item);
-    }
+function loadCardsListFromItemsController() {
+    const items = itemsController.getItems();
+    items.forEach(item => addItemCard(item));
 }
 
 loadStorageSampleData();
