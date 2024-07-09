86476719255f114c25d1d3378c3f830f2e385420
Mariana Lopez <marianaalexa76@gmail.com>
2024-07-09 17:27:33 -0600

    modify: se cambio de lugar external en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        'src/pages/productos/itemsController.js', // Sustituye 'nombre-del-modulo-problematico' con el nombre real del módulo
-      ],
       input: {
         main: resolve(__dirname, 'index.html'),
         aboutUs: resolve(__dirname, 'src/pages/acercaDeNosotros/acercadenosotros.html'),
         products: resolve(__dirname, 'src/pages/productos/productos.html'),
         contact: resolve(__dirname, 'src/pages/contacto/contacto.html'),
       },
+      external: [
+        'src/pages/productos/itemsController.js', // Sustituye 'nombre-del-modulo-problematico' con el nombre real del módulo
+      ],
     },
   },
 })
